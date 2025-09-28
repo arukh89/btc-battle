006848eade9957a33259d28953e8308424d2e678
arukh89 <85455057+arukh89@users.noreply.github.com>
2025-09-28 16:41:10 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',              // pastikan root project benar
   build: {
-    outDir: "dist"
+    outDir: 'dist',       // hasil build ke dist
+    emptyOutDir: true
   }
-});
+})
